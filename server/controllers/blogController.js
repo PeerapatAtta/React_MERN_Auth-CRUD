4d9820ee3e2d54ee63edef4073eeffdfd2d5ce7f
OFF <peerapat13th@gmail.com>
2025-05-16 00:20:04 +0700

    14. Slugify

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -2,8 +2,16 @@
 // exports.create = (req, res) => {
 //     res.json({ data: "message from server - blogController" });
 // }
+const slugify = require('slugify');
 
 //Save data
 exports.create = (req, res) => {
-    res.json({ data: req.body });
+    const { title, content, author} = req.body
+    const slug = slugify(req.body.slug)
+
+    res.json({ 
+        data: { title, content, author, slug }
+    });
 }
+
+//localhost:8000/api/blog/create
--- server/routes/blog.js
+++ server/routes/blog.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {create} = require('../controllers/blogController');
 
 // router.get('/blog', create);
-router.post('/blog', create);
+router.post('/create', create);
 
 module.exports = router;
